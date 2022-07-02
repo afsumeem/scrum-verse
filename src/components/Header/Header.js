6438d7221a2d4ca1fa221eb560f908cdd507b753
Afsana Meem <afsumeem@gmail.com>
2022-07-02 23:28:50 +0600

    headed updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Button, Nav, Navbar } from 'react-bootstrap';
+import logo1 from "../../images/logo1.svg"
+import logo2 from "../../images/logo2.svg"
+import logo3 from "../../images/logo3.svg"
+import "./Header.css";
 
 const Header = () => {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" variant="dark" className='py-4 px-4'>
 
-                <Navbar.Brand href="#home">SCRUMVERSE</Navbar.Brand>
+                <Navbar.Brand href="#home" className="logodiv">
+                    <div className='logo me-4'>
+                        <img src={logo1} className="logo1" alt="" />
+                        <img src={logo2} className="logo2" alt="" />
+                        <img src={logo3} className="logo3" alt="" />
+                        <p className='logoTitle'
+
+                            style={{ fontWeight: "700", fontSize: "14px" }}>SCRUMVERSE</p>
+                    </div>
+
+                </Navbar.Brand>
+
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mx-auto">
-                        <Nav.Link href="#home" className='text-white'>HOME</Nav.Link>
-                        <Nav.Link href="#about" className='text-white'>ABOUT</Nav.Link>
-                        <Nav.Link href="#courses" className='text-white'>COURSES</Nav.Link>
-                        <Nav.Link href="#blog" className='text-white'>BLOG</Nav.Link>
+                        <Nav.Link href="#home" className='text-white'
+                            style={{ fontWeight: "700", fontSize: "14px" }}
+                        >HOME</Nav.Link>
+                        <Nav.Link href="#about" className='text-white'
+                            style={{ fontWeight: "700", fontSize: "14px" }}
+                        >ABOUT</Nav.Link>
+                        <Nav.Link href="#courses" className='text-white'
+                            style={{ fontWeight: "700", fontSize: "14px" }}
+                        >COURSES</Nav.Link>
+                        <Nav.Link href="#blog" className='text-white'
+                            style={{ fontWeight: "700", fontSize: "14px" }}
+                        >BLOG</Nav.Link>
 
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets" className='text-white'>LOGIN</Nav.Link>
-                        <Button variant="outline-light">SIGN UP</Button>
+                        <Nav.Link
+                            style={{ fontWeight: "700", fontSize: "14px" }}
+                            href="#deets" className='text-white'>LOGIN</Nav.Link>
+                        <Button variant="outline-light rounded-pill border border-2"
+                            style={{ fontWeight: "700", fontSize: "14px" }}
+                        >SIGN UP</Button>
                     </Nav>
                 </Navbar.Collapse>
 
