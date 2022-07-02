a17b72a942a96b5cce0d7fd89c4e0c73e41e5953
Afsana Meem <afsumeem@gmail.com>
2022-07-02 19:51:26 +0600

    facilities design

--- src/components/Home/Facilities/Facilities.js
+++ src/components/Home/Facilities/Facilities.js
@@ -1,10 +1,82 @@
 import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import "./Facilities.css"
+import examImg from "../../../images/exam.svg";
+import lesson from "../../../images/lesson.svg";
+import lesson2 from "../../../images/lesson2.svg";
+import liveClasses from "../../../images/liveClasses.svg"
+
+
 
 const Facilities = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Row>
+                <Col md={5} className="d-flex flex-column align-self-center">
+                    <h2 className='facility-title'>
+                        Start learning with <br /> us right now!
+                    </h2>
+                    <p className='facility-content'>
+                        Choose from 100+ online video <br /> courses with new updates.
+                    </p>
+                    <Button variant="rounded-pill w-25 enroll-now fw-bold">
+                        ENROLL NOW
+                    </Button>
+                </Col>
+                <Col md={7}>
+                    <Row>
+                        <Col>
+                            <div className='d-flex align-items-center' style={{ marginBottom: "50px" }}>
+                                <h5 className='section-title'>
+                                    Live Exams
+                                </h5>
+                                <div className="exams">
+                                    <img src={examImg} alt="" />
+                                </div>
+                            </div>
+                            <div className='d-flex align-items-center' style={{ marginBottom: "50px" }}>
+                                <h5 className='section-title'>
+                                    LIVE Classes
+                                </h5>
+                                <div className="exams">
+                                    <img src={examImg} alt="" />
+                                </div>
+                            </div>
+                            <div className='d-flex align-items-center'>
+                                <h5 className='section-title'>
+                                    100 k+ Views
+                                </h5>
+                                <div className="exams">
+                                    <img src={examImg} alt="" />
+                                </div>
+                            </div>
+
+                        </Col>
+                        <Col className='d-flex flex-column align-self-center'>
+                            <div className='d-flex align-items-center mb-4' >
+                                <h5 className='section-title'>
+                                    1000+ Lessons
+                                </h5>
+                                <div className="exams">
+                                    <img src={examImg} alt="" />
+                                </div>
+                            </div>
+                            <div className='d-flex align-items-center mt-4'>
+                                <h5 className='section-title'>
+                                    1000+ Lessons
+                                </h5>
+                                <div className="exams">
+                                    <img src={examImg} alt="" />
+                                </div>
+                            </div>
+
+                        </Col>
+                    </Row>
+                </Col>
+
+            </Row>
+        </Container>
+
     );
 };
 
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Courses from '../Courses/Courses/Courses';
+import Facilities from '../Facilities/Facilities';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Courses />
+            <Facilities />
         </div>
     );
 };
