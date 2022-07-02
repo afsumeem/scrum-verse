94deba4e5ec7429ee3bf34db56ab4bd93c50605f
Afsana Meem <afsumeem@gmail.com>
2022-07-02 16:53:58 +0600

    courses section done

--- src/components/Home/Courses/Course/Course.js
+++ src/components/Home/Courses/Course/Course.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { Button, Card, Col } from 'react-bootstrap';
+import "./Course.css"
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const Course = ({ course }) => {
+    const star = <FontAwesomeIcon icon={faStar} />
     return (
-        <div>
-            <h1>{course.name}</h1>
+        <Col md={3} sm={6} sx={12} style={{ marginTop: "65px", marginBottom: "65px" }}>
+            <Card className='border border-0 course'>
+                <div className='course-image'>
+                    <Card.Img variant="top" src={course.image} />
+                </div>
 
-        </div>
+                <Card.Body className="course-content">
+                    <Card.Title className='course-title'>
+                        Learn SCRUM: The Complete Beginner Course
+                    </Card.Title>
+
+                    <p className='course-name'>
+                        {course.name}
+                    </p>
+                    <div className='d-flex'>
+                        <p className='course-rating'>
+                            {course.rating}
+                        </p>
+                        <p className='course-rating-star ms-3'>{star}{star}{star}{star}{star}</p>
+                    </div>
+                    <Button variant="rounded-pill w-50 enroll-now fw-bold">
+                        ENROLL NOW
+                    </Button>
+                </Card.Body>
+
+
+
+            </Card>
+        </Col>
     );
 };
 
--- src/components/Home/Courses/Courses/Courses.js
+++ src/components/Home/Courses/Courses/Courses.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
 import Course from '../Course/Course';
 
 const Courses = () => {
@@ -10,14 +11,19 @@ const Courses = () => {
     }, []);
 
     return (
-        <div>
-            {
-                courses.map(course => <Course key={course._id}
-                    course={course}
-                >
-                </Course>)
-            }
+        <div className='px-5'>
+            <p style={{ marginTop: "95px", fontSize: "18px", fontWeight: "700" }}>A broad range of courses</p>
+            <p style={{ fontSize: "18px", fontWeight: "400" }}>Choose from 100+ online video courses with new additions published every month</p>
+            <Row>
+                {
+                    courses.map(course => <Course key={course._id}
+                        course={course}
+                    >
+                    </Course>)
+                }
+            </Row>
         </div>
+
     );
 };
 
